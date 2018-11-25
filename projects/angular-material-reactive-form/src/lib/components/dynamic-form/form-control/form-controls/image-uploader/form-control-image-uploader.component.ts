a8da88be60707a92b14c579e9b7b23349ceecfb6
Maciej Żółtowski <maciej.zoltowski@e-qsg.com>
2018-11-25 10:43:00 +0100

    fixes

--- projects/angular-material-reactive-form/src/lib/components/dynamic-form/form-control/form-controls/image-uploader/form-control-image-uploader.component.ts
+++ projects/angular-material-reactive-form/src/lib/components/dynamic-form/form-control/form-controls/image-uploader/form-control-image-uploader.component.ts
@@ -1,116 +1,112 @@
 import { Component, forwardRef, Input } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
-import {
-  FileItem,
-  FileUploader,
-  ParsedResponseHeaders
-} from 'ng2-file-upload/';
-import { ImageModel } from 'projects/angular-material-reactive-form/src/lib/model/models/image.model';
-import { ImageInterface } from 'projects/angular-material-reactive-form/src/lib/model/interfaces/image.interface';
+import { FileItem, FileUploader, ParsedResponseHeaders } from 'ng2-file-upload/';
 import { FormCustomizationInterface } from 'projects/angular-material-reactive-form/src/lib/model/interfaces/form-customization.interface';
+import { ImageInterface } from 'projects/angular-material-reactive-form/src/lib/model/interfaces/image.interface';
+import { ImageModel } from 'projects/angular-material-reactive-form/src/lib/model/models/image.model';
 import { RequestResponseModel } from 'projects/angular-material-reactive-form/src/lib/model/models/request-response.model';
 import { AbstractFormComponent } from '../../../../abstract-form/abstract-from.component';
 const URL = 'http://localhost:3000/api/images/';
 
 @Component({
-  selector: 'form-control-image-uploader',
-  templateUrl: './form-control-image-uploader.component.html',
-  styleUrls: ['./form-control-image-uploader.component.css'],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => FormControlImageUploaderComponent),
-      multi: true
-    }
-  ]
+	selector: 'form-control-image-uploader',
+	templateUrl: './form-control-image-uploader.component.html',
+	styleUrls: ['./form-control-image-uploader.component.css'],
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			useExisting: forwardRef(() => FormControlImageUploaderComponent),
+			multi: true
+		}
+	]
 })
 export class FormControlImageUploaderComponent<T = ImageModel>
-  extends AbstractFormComponent
-  implements ControlValueAccessor {
-  public uploader: FileUploader;
-  @Input()
-  public set customization(customization: FormCustomizationInterface) {
-    this.uploader = new FileUploader({
-      url: `${URL}/${customization.directory}/${customization.id}`
-    });
-  }
+	extends AbstractFormComponent
+	implements ControlValueAccessor {
+	public uploader: FileUploader;
+	@Input()
+	public set customization(customization: FormCustomizationInterface) {
+		this.uploader = new FileUploader({
+			url: `${URL}/${customization.directory}/${customization.id}`
+		});
+	}
 
-  @Input() public disabled = false;
+	@Input() public disabled = false;
 
-  public hasBaseDropZoneOver: boolean = false;
-  public images: ImageModel[] = [];
-  public onChange = (images: ImageInterface[]) => {};
-  public onTouched = () => {};
+	public hasBaseDropZoneOver: boolean = false;
+	public images: ImageModel[] = [];
+	public onChange = (images: ImageInterface[]) => {};
+	public onTouched = () => {};
 
-  public get value(): ImageModel[] {
-    return [...this.images];
-  }
+	public get value(): ImageModel[] {
+		return [...this.images];
+	}
 
-  public writeValue(images: ImageModel[]): void {
-    this.images = (images || []).map(item => new ImageModel(item));
-    this.onChange(this.value);
-  }
+	public writeValue(images: ImageModel[]): void {
+		this.images = (images || []).map(item => new ImageModel(item));
+		this.onChange(this.value);
+	}
 
-  public addValue(image: ImageModel) {
-    this.writeValue([...this.value, image]);
-  }
+	public addValue(image: ImageModel) {
+		this.writeValue([...this.value, image]);
+	}
 
-  public registerOnChange(fn: (images: ImageInterface[]) => void): void {
-    this.onChange = fn;
-  }
+	public registerOnChange(fn: (images: ImageInterface[]) => void): void {
+		this.onChange = fn;
+	}
 
-  public registerOnTouched(fn: () => void): void {
-    this.onTouched = fn;
-  }
+	public registerOnTouched(fn: () => void): void {
+		this.onTouched = fn;
+	}
 
-  public setDisabledState(isDisabled: boolean): void {
-    this.disabled = isDisabled;
-  }
+	public setDisabledState(isDisabled: boolean): void {
+		this.disabled = isDisabled;
+	}
 
-  public fileOverBase(event): void {
-    this.hasBaseDropZoneOver = event;
-  }
+	public fileOverBase(event): void {
+		this.hasBaseDropZoneOver = event;
+	}
 
-  public handleUploadAll() {
-    this.uploader.uploadAll();
-  }
+	public handleUploadAll() {
+		this.uploader.uploadAll();
+	}
 
-  public handleCancelAll() {
-    this.uploader.cancelAll();
-  }
+	public handleCancelAll() {
+		this.uploader.cancelAll();
+	}
 
-  public handleClearQueue() {
-    this.uploader.clearQueue();
-  }
+	public handleClearQueue() {
+		this.uploader.clearQueue();
+	}
 
-  public handleUpload(item) {
-    this.uploader.onSuccessItem = this.onSuccessItem.bind(this);
-    item.upload();
-  }
+	public handleUpload(item) {
+		this.uploader.onSuccessItem = this.onSuccessItem.bind(this);
+		item.upload();
+	}
 
-  public handleCancel(item) {
-    item.cancel();
-  }
+	public handleCancel(item) {
+		item.cancel();
+	}
 
-  public handleRemove(item) {
-    item.remove();
-  }
+	public handleRemove(item) {
+		item.remove();
+	}
 
-  public onSuccessItem(
-    item: FileItem,
-    response: string,
-    status: number,
-    headers: ParsedResponseHeaders
-  ) {
-    if (!response) {
-      return;
-    }
+	public onSuccessItem(
+		item: FileItem,
+		response: string,
+		status: number,
+		headers: ParsedResponseHeaders
+	) {
+		if (!response) {
+			return;
+		}
 
-    const responseModel = new RequestResponseModel(JSON.parse(response));
-    const image = new ImageModel(responseModel.payload);
+		const responseModel = new RequestResponseModel(JSON.parse(response));
+		const image = new ImageModel(responseModel.payload);
 
-    if (image) {
-      this.addValue(image);
-    }
-  }
+		if (image) {
+			this.addValue(image);
+		}
+	}
 }
--- projects/angular-material-reactive-form/src/lib/components/dynamic-form/form-control/form-controls/input/form-control-input.component.ts
+++ projects/angular-material-reactive-form/src/lib/components/dynamic-form/form-control/form-controls/input/form-control-input.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { AbstractFormComponent } from '../../../../abstract-form/abstract-from.component';
 import { FormGroup } from '@angular/forms';
 import { FormControlModel } from 'projects/angular-material-reactive-form/src/lib/model/models/form-control.model';
+import { InputTypeEnum } from 'projects/angular-material-reactive-form/src/lib/model/enums/form.enums';
 
 @Component({
   selector: 'form-control-input',
@@ -11,4 +12,5 @@ import { FormControlModel } from 'projects/angular-material-reactive-form/src/li
 export class FormControlInputComponent<T> extends AbstractFormComponent {
 	@Input() public control: FormControlModel<T>;
 	@Input() public form: FormGroup;
+	public inputTypeEnum = InputTypeEnum;
 }
