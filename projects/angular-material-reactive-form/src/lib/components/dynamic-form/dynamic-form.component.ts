f72951b91e9ce595c973e8f586673fa032d929b7
Maciej Żółtowski <maciej.zoltowski@e-qsg.com>
2018-11-25 11:25:43 +0100

    fixes

--- projects/angular-material-reactive-form/src/lib/components/dynamic-form/dynamic-form.component.ts
+++ projects/angular-material-reactive-form/src/lib/components/dynamic-form/dynamic-form.component.ts
@@ -6,42 +6,42 @@ import { FormControlsInterface } from '../../model/interfaces/form-controls.inte
 import { AbstractFormComponent } from '../abstract-form/abstract-from.component';
 
 @Component({
-  selector: 'app-dynamic-form',
-  templateUrl: './dynamic-form.component.html',
-  styleUrls: ['./dynamic-form.component.css']
+	selector: 'app-dynamic-form',
+	templateUrl: './dynamic-form.component.html',
+	styleUrls: ['./dynamic-form.component.css']
 })
 export class DynamicFormComponent extends AbstractFormComponent {
-  @Input()
-  public formPath: Array<string | number> = [];
-  @Input()
-  public controls: FormControlsInterface;
-  @Input()
-  public form: FormGroup;
-  @Input()
-  public loader: boolean;
-  @Output()
-  public onSubmit: EventEmitter<NgForm> = new EventEmitter<NgForm>();
-  public controlTypeEnum = ControlTypeEnum;
+	@Input()
+	public formPath: Array<string | number> = [];
+	@Input()
+	public controls: FormControlsInterface;
+	@Input()
+	public form: FormGroup;
+	@Input()
+	public loader: boolean;
+	@Output()
+	public onSubmit: EventEmitter<FormGroup> = new EventEmitter<FormGroup>();
+	public controlTypeEnum = ControlTypeEnum;
 
-  constructor(public route: ActivatedRoute, public router: Router) {
-    super();
-  }
+	constructor(public route: ActivatedRoute, public router: Router) {
+		super();
+	}
 
-  public onFormSubmit(form: NgForm) {
-    this.onSubmit.emit(form);
-  }
+	public onFormSubmit(form: FormGroup) {
+		this.onSubmit.emit(form);
+	}
 
-  public handleDetails() {
-    this.router.navigate(['..'], { relativeTo: this.route });
-  }
+	public handleDetails() {
+		this.router.navigate(['..'], { relativeTo: this.route });
+	}
 
-  public handleList(form: NgForm) {
-    const {
-      value: { id = '' }
-    } = form;
-    if (!id) {
-      this.router.navigate(['../'], { relativeTo: this.route });
-    }
-    this.router.navigate(['../../'], { relativeTo: this.route });
-  }
+	public handleList(form: FormGroup) {
+		const {
+			value: { id = '' }
+		} = form;
+		if (!id) {
+			this.router.navigate(['../'], { relativeTo: this.route });
+		}
+		this.router.navigate(['../../'], { relativeTo: this.route });
+	}
 }
--- projects/angular-material-reactive-form/src/lib/model/enums/form.enums.ts
+++ projects/angular-material-reactive-form/src/lib/model/enums/form.enums.ts
@@ -1,41 +1,41 @@
 export enum ControlTypeEnum {
-  FormHidden,
-  FormControl,
-  FormArray,
-  FormGroup
+	FormHidden,
+	FormControl,
+	FormArray,
+	FormGroup
 }
 
 export enum FormControlTypeEnum {
-  INPUT,
-  AUTOCOMPLETE,
-  CHECKBOX,
-  DATEPICKER,
-  IMAGE,
-  MULTISELECT,
-  RADIO,
-  SELECT,
-  SLIDER,
-  TEXTAREA,
-  TOGGLE
+	INPUT,
+	AUTOCOMPLETE,
+	CHECKBOX,
+	DATEPICKER,
+	IMAGE,
+	MULTISELECT,
+	RADIO,
+	SELECT,
+	SLIDER,
+	TEXTAREA,
+	TOGGLE
 }
 
 export enum InputTypeEnum {
-  text = 'text',
-  color = 'color',
-  date = 'date',
-  datetimeLocal = 'datetime-local',
-  email = 'email',
-  month = 'month',
-  number = 'number',
-  password = 'password',
-  search = 'search',
-  tel = 'tel',
-  time = 'time',
-  url = 'url',
-  week = 'week'
+	text = 'text',
+	color = 'color',
+	date = 'date',
+	datetimeLocal = 'datetime-local',
+	email = 'email',
+	month = 'month',
+	number = 'number',
+	password = 'password',
+	search = 'search',
+	tel = 'tel',
+	time = 'time',
+	url = 'url',
+	week = 'week'
 }
 
 export enum InputFieldSuffix {
-  cm = 'cm',
-  mm = 'mm',
+	cm = 'cm',
+	mm = 'mm'
 }
